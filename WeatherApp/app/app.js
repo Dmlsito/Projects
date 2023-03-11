8985d161f106d5542bcd898b3f87cf352a00f186
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-03-11 13:48:37 +0100

    Search nav and transition to the mainWeather window

---
+++ WeatherApp/app/app.js
@@ -0,0 +1,9 @@
+const closeIcon = document.querySelector(".principal-info-weather-data__search__icon");
+const search = document.querySelector(".principal-info-weather-data__search");
+const cardMainWeather = document.querySelector(".principal-info-weather-data");
+
+closeIcon.addEventListener("click", () => {
+   search.style.marginTop = "-100vh";
+  
+
+})
