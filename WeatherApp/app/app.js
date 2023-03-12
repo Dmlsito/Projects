8c38ba6c5463adc5a2d7e5dde0838bd7c527881a
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-03-12 20:15:38 +0100

    Changes at app.js

--- WeatherApp/app/app.js
+++ WeatherApp/app/app.js
@@ -1,9 +1,40 @@
-const closeIcon = document.querySelector(".principal-info-weather-data__search__icon");
+const closeIconSearch = document.querySelector(".principal-info-weather-data__search__icon");
 const search = document.querySelector(".principal-info-weather-data__search");
-const cardMainWeather = document.querySelector(".principal-info-weather-data");
+const cardMainWeather = document.querySelector(".principal-info-weather-data__info");
+const btnSearch = document.querySelector(".principal-info-weather-data__info-btnSearch");
+const searhInput = document.getElementById("searchInput");
+const mainBtnSearch = document.getElementById("mainButton")
 
-closeIcon.addEventListener("click", () => {
-   search.style.marginTop = "-100vh";
+//Method to get the data
+const getData = () => {
   
+  let latitude;
+  let longitude;
+
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&appid=9a81ed26c2118da6383448260eeacebe`)
+    .then(res => res.json())
+    .then(res => {
+      //This data is very important to search the weather in each city
+     latitude = res[0].lat;
+     longitude = res[0].lon;
+     
+     fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=9a81ed26c2118da6383448260eeacebe`)
+     .then(res => res.json())
+     .then(res => console.log(res))
+    })
+}
+
+closeIconSearch.addEventListener("click", () => {
+  cardMainWeather.style.display = "flex";
+  search.style.display = "none";
 
 })
+
+btnSearch.addEventListener("click", () => {
+   cardMainWeather.style.display = "none";
+   search.style.display = "flex";
+})
+mainButton.addEventListener("click", getData);
+
+
+
