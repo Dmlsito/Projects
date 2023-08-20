6f433a956316551d1a030ba2de3d5c325a12f9a1
daniMallo <101351451+Dmlsito@users.noreply.github.com>
2023-08-20 12:18:14 +0200

    ToDo Day 2
    Se ha creado la logica de guardado de notas, arreglado algunos estilos y nada mas, falta por pulir la localizacion de la informaicon de las notas.
    Provisionalmente, se dejara la logica en un archivo, mas adelante se extraera y se refactorizara la mayoria del codigo para su reutilizacion.

--- ToDo/front/src/components/home.jsx
+++ ToDo/front/src/components/home.jsx
@@ -1,29 +1,60 @@
 import './home.css'
+import { useState, useEffect, useRef} from 'react'
 
 const home = () => {
 
+    const [textAreaInfo, setTextAreaInfo] = useState(new Array())
+    const [existInfo, setExistInfo] = useState(false)
+
+    const textAreaRef = useRef(null)
+
+    const saveNote = () => {
+        
+        setTextAreaInfo(info => {
+            return [...info, textAreaRef.current.value]
+        })
+        setExistInfo(true)
+
+    }
+
     return (
+
        <div className='provisional'>
 
             <header>
                 <h1>To do</h1>
             </header>
-            
+
             <div className='main'>
+
                 <main className='main-container'>
                 
                         <div className='main-article-dateContainer'>
-                            <h3>Dia del mes</h3>
+                            <h3>{`Dia -> ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}  `}</h3>
                         </div>
                         <div className='main-article-input'>
-                            <textarea></textarea>
+                            <textarea ref={textAreaRef} />
                         </div>
-                    
+
+                        <button onClick={saveNote}>Save note</button> 
                 </main>
 
                 <aside className='main-aside'>
-                    <h2>Informacion adicional</h2>
+                    <h2>Today's notes</h2>
+
+                   
+
+                    { existInfo && 
+                        <div className='main-aside-containerNotes'>
+                      {textAreaInfo.map((infoNote, i) => (
+                        <span key={i}>{infoNote}</span>
+                      )) 
+                      }</div>
+                    }
+                  
+
                 </aside>
+                
             </div>
 
         </div>
