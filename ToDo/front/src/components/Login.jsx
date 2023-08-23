e8ba19d079ee9ff2aef32c969baf734519e3768e
daniMallo <101351451+Dmlsito@users.noreply.github.com>
2023-08-23 12:40:53 +0200

    Validador de usuarios en el front realizada

--- ToDo/front/src/components/Login.jsx
+++ ToDo/front/src/components/Login.jsx
@@ -1,27 +1,46 @@
 import './Login.css'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const Login = () => {
 
-  const username = new useRef(null)
-  const password = new useRef(null)
+  const username = new useRef()
+  const password = new useRef()
 
-  const handleClick = async (e) => {
-    const { error } = await fetch()
-    e.preventDefault()
+  const [errorLogin, setErrorLogin] = useState(null)
+  const [showErrors, setShowErrors] = useState(false)
+
+  const checkUser =  async () => {
+   let user = {'username': username.current.value, 'password': password.current.value}
+   const error = await fetch("http://localhost:8080/api/v1/user/checkUser", {
+    method: "POST",   
+    body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json())
+
+    setErrorLogin(error !== 0 ? false: true)
   }
 
+  const handleClick = (e) => {
+    if(errorLogin) {
+      e.preventDefault()
+      setShowErrors(true)
+  }
+}
 
   return(
     <main className='login'>
       <form className='login-form' action='/home' onSubmit={handleClick}>
       <h1>To doo</h1>
-        <input placeholder='username' ref={username}></input>
-        <input placeholder='password' ref={password}></input>
+        <input placeholder='username' ref={username} required="yes" onBlur={checkUser}></input>
+        <input placeholder='password' ref={password} required="yes" onBlur={checkUser}></input>
+        {showErrors && <span className='error'>Usuario incorrecto</span>}
         <button type='submit'>Sign in</button>
       </form>
     </main>
   )
 }
+
 
 export default Login
--- ToDo/server/demo/src/main/java/com/example/demo/Controller/UserController.java
+++ ToDo/server/demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -18,26 +18,32 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/getAll")
     public ResponseEntity<?> queryAllUsers() {
         return new ResponseEntity<>(userService.queryAllUsers(), HttpStatus.resolve(200));
     }
 
+
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/getUser/{id}")
     public HashMap<String, String> queryUser(@PathVariable Integer id) {
         return userService.convertUserToHashMap(userService.queryUser(id));
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/createUser")
     public ResponseEntity<?> insertUser(@RequestBody User user) {
         return new ResponseEntity<>(userService.insertUser(user), HttpStatus.resolve(200));
     }
 
-    @GetMapping("/checkUser")
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/checkUser")
     public ResponseEntity<?> checkUser(@RequestBody User user) {
         return new ResponseEntity<>(userService.checkUser(user), HttpStatus.resolve(200));
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @DeleteMapping("/deleteUser/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Integer id) {
         return new ResponseEntity<>(userService.deleteUser(id), HttpStatus.resolve(200));
